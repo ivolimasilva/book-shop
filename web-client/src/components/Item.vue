7f8ceff88f6d8f80f82b1915f2cb5d1b0d446452
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-05-14 23:47:25 +0100

    Initial dashboard + item component.

---
+++ web-client/src/components/Item.vue
@@ -0,0 +1,37 @@
+<template>
+    <article class="tile is-child notification is-info">
+        <div class="columns">
+            <div class="column">
+                <figure class="image">
+                    <img class="img-small" src="https://unsplash.it/400/500">
+                </figure>
+            </div>
+            <div class="column flex flex-column">
+                <p class="title">Title</p>
+                <p class="subtitle">Author</p>
+                <p class="price flex-end has-text-right">10.00€</p>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'item'
+}
+</script>
+
+<style scoped>
+.flex {
+    display: flex;
+}
+
+.flex-column {
+    flex-direction: column;
+}
+
+.price {
+    margin-top: auto;
+    font-size: 2em;
+}
+</style>
--- web-client/src/views/Dashboard.vue
+++ web-client/src/views/Dashboard.vue
@@ -1,12 +1,62 @@
 <template>
+    <div class="container">
+        <div class="tile is-ancestor">
+            <div class="tile is-parent is-8">
+                <article class="tile is-child notification is-info">
+                    <div class="columns">
+                        <div class="column">
+                            <figure class="image">
+                                <img class="img-big" src="https://unsplash.it/300/500">
+                            </figure>
+                        </div>
+                        <div class="column flex flex-column">
+                            <a>
+                                <p class="title">Title</p>
+                            </a>
+                            <p class="subtitle">Author</p>
+                            <p class="price flex-end has-text-right">10.00€</p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div class="tile is-vertical">
+                <div class="tile">
+                    <div class="tile is-parent is-vertical">
+                        <item></item>
+                        <item></item>
+                        <item></item>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Item from '../components/Item.vue';
+
 export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    components: {
+        'item': Item
+    }
 }
 </script>
 
 <style scoped>
+.container {
+    margin-top: 25px;
+}
 
+.flex {
+    display: flex;
+}
+
+.flex-column {
+    flex-direction: column;
+}
+
+.summary {
+    text-align: justify;
+}
 </style>
