b0a5b5c7fa1dcfe42cd74a2b05656539ce20ae6e
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-05-15 01:14:04 +0100

    Refactor of the GET /book and added verifications when querying the DB.

--- store-server/routes/book/index.js
+++ store-server/routes/book/index.js
@@ -1,4 +1,19 @@
+'use strict'
+
 module.exports = function (server) {
 
-    require('routes/book/book')(server);
+    /*
+     * Route to get books by (optional) filter
+     * Method:  GET
+     * Params:  _id - Book's ID (not required)
+     *          year - Book's Year (not required)
+     *          ...
+     *          (doesn't work with queries other than equal, like 'less than' (<), 'greater than' (>), ...)
+     *          (no filter returns all the books)
+     *
+     * Returns: the book's information
+     */
+
+    require('routes/book/get')(server);
+
 };
