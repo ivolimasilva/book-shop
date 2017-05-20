ee40080ca66223454bcada9014ad4a4c09c76132
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-05-20 15:29:50 +0100

    Initial warehouse server configuration.

---
+++ warehouse-server/index.js
@@ -0,0 +1,79 @@
+'use strict'
+
+var Hapi = require('hapi'),
+    Config = require('config');
+
+// Create a server with validation
+var server = new Hapi.Server({
+    app: {
+        validation: {
+            allowUnknown: true
+        }
+    }
+});
+
+// Set the port and CORS to true
+server.connection({
+    port: Config.server.port,
+    routes: {
+        cors: {
+            origin: ['*'],
+            // headers: ['*'],
+            credentials: true
+        }
+    }
+});
+
+// Log (to console & file) configuration
+const options = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        console: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{
+                log: '*',
+                response: '*'
+            }]
+        }, {
+            module: 'good-console'
+        }, 'stdout'],
+        file: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{
+                log: '*',
+                response: '*'
+            }]
+        }, {
+            module: 'good-squeeze',
+            name: 'SafeJson'
+        }, {
+            module: 'good-file',
+            args: ['./logs/' + new Date().getTime() + '.json']
+        }]
+    }
+};
+
+// Routes
+// require('routes')(server);
+
+// Register and if no errors start the server
+server.register({
+    register: require('good'),
+    options
+}, (err) => {
+
+    // Error loading the configuration
+    if (err) {
+        return console.error(err);
+    }
+
+    // Starting the server
+    server.start(() => {
+        console.info('Server started @', server.info.uri);
+    });
+
+});
