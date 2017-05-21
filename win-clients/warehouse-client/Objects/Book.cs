e866f88f9b6905533c1f1b5cc958c6dcfc99680c
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-05-21 23:06:44 +0100

    List stock.

---
+++ win-clients/warehouse-client/Objects/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace warehouse_client.Objects
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+
+        public string isbn { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public int year { get; set; }
+        public string publisher { get; set; }
+        public float price { get; set; }
+        public int stock { get; set; }
+        public string image { get; set; }
+    }
+}
