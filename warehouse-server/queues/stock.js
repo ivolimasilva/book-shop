4dfd4b976e388a58b5e456ba6802be81797b40fe
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-05-22 02:04:31 +0100

    Stock request queue working.

--- warehouse-server/queues/stock.js
+++ warehouse-server/queues/stock.js
@@ -7,13 +7,18 @@ module.exports = function (worker) {
 
     worker.on('message', function (msg, next, id) {
 
-        var stock = new Stock(JSON.parse(msg.message));
+        var message = JSON.parse(msg),
+            stock = new Stock({
+                isbn: message.isbn,
+                quantity: message.quantity,
+                _id_order: message._id_order
+            });
 
         stock.save((err, _stock) => {
             if (err) {
                 console.log('Server error saving Stock Request.');
             } else {
-                console.log('Saved Stock Request.');
+                console.log('Saved Stock Request: ' + _stock._id);
             }
         });
 
